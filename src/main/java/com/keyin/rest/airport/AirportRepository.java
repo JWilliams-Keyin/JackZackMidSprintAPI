721b73ee9cd6674fa9907f2fede60b23feaed0ad
JWilliams-Keyin <Jack.Williams@keyin.com>
2025-06-20 13:05:59 -0230

    Fixed another typo in AirportRepository.java

--- src/main/java/com/keyin/rest/airport/AirportRepository.java
+++ src/main/java/com/keyin/rest/airport/AirportRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AirportRepository extends CrudRepository<Aircraft, Long> {
+public interface AirportRepository extends CrudRepository<Airport, Long> {
     public Airport findByAirportName(String airportName);
 
     public Airport findByAirportCode(String airportCode);
